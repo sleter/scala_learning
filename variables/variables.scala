d8a8eeef7dc5ae5c638a04700ecbc06c583bedc4
sleter <sleter97@gmail.com>
2019-03-05 19:48:34 +0100

    scala basics

---
+++ variables/variables.scala
@@ -0,0 +1,11 @@
+object Var {
+   def main(args: Array[String]) {
+      var myVar :Int = 1337;
+      val myVal :String = "Yo";
+      var myVar1 = 7331;
+      val myVal1 = "oY";
+      
+      println(myVar); println(myVal); println(myVar1); 
+      println(myVal1);
+   }
+}
